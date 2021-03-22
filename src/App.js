6ad94d812cfcd875ff7f8f34e672c478036b214f
Scott Campbell <thisisscottcampbell@gmail.com>
2021-03-22 00:54:26 -0400

    render and limit list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 
-const App = () => {
-	return <div>i am app</div>;
+const App = ({ memes }) => {
+	const [limit, setLimit] = useState(10);
+
+	const listMemes = memes
+		.slice(0, limit)
+		.map((meme, i) => <h4 key={i}>{meme.name}</h4>);
+	return (
+		<div>
+			<h3>MemeGenerator!!!</h3>
+			<div>{listMemes}</div>
+		</div>
+	);
 };
 
-export default App;
+const mapStateToProps = (state) => {
+	return { memes: state.memes };
+};
+export default connect(mapStateToProps, null)(App);
